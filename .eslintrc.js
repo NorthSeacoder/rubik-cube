a2714f0ebc236aaa2591ca48aaf3e888f1cd0661
mengpeng <pengmeng@yangqianguan.com>
2019-05-13 10:33:14 +0800

    opt

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,14 +6,12 @@ module.exports = {
   },
   extends: 'airbnb-base',
   // required to lint *.vue files
-  plugins: [
-    'html'
-  ],
+  plugins: ['html'],
   // check if imports actually resolve
-  'settings': {
+  settings: {
     'import/resolver': {
-      'webpack': {
-        'config': 'build/webpack.base.conf.js'
+      webpack: {
+        config: 'build/webpack.base.conf.js'
       }
     }
   },
@@ -22,29 +20,38 @@ module.exports = {
     browser: true,
     node: true
   },
-  'rules': {
+  rules: {
     'arrow-parens': 'off',
+    'object-curly-spacing': 'off',
+    indent: 'off',
     'func-names': [0, 'never'],
-    'max-len': [0, 80],
+    'max-len': [0, 120],
     'prefer-spread': 0,
     'no-console': 0,
-    'semi': 0,
+    semi: 0,
     'no-plusplus': 0,
     'comma-dangle': 0,
-    'eqeqeq': 0,
+    eqeqeq: 0,
     'no-param-reassign': 0,
     'space-before-function-paren': [1, 'never'],
     'arrow-body-style': 0,
-    'import/no-extraneous-dependencies': [0, {
-      'devDependencies': ['/test/**/*.js']
-    }],
+    'import/no-extraneous-dependencies': [
+      0,
+      {
+        devDependencies: ['/test/**/*.js']
+      }
+    ],
     // don't require .vue extension when importing
-    'import/extensions': ['error', 'always', {
-      'js': 'never',
-      'vue': 'never'
-    }],
+    'import/extensions': [
+      'error',
+      'always',
+      {
+        js: 'never',
+        vue: 'never'
+      }
+    ],
 
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
-}
+};
